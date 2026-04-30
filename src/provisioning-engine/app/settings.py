a929980c0c7e4cbe910974ec117685c9b00a53a1
xavor <xavor@hotmail.es>
2026-04-30 12:16:35 +0000

    desliar

--- src/provisioning-engine/app/settings.py
+++ src/provisioning-engine/app/settings.py
@@ -1,5 +1,5 @@
 import os
 
-DS_URL = os.environ.get("DS_URL", "http://192.168.1.70:8081")
-TANG_URL = os.environ.get("TANG_URL", "http://192.168.1.70:8082")
+DS_URL = os.environ.get("DS_URL", "https://boot.hermes.ranabo.com")
+TANG_URL = os.environ.get("TANG_URL", "https://tang.hermes.ranabo.com")
 REPO_URL = os.environ.get("REPO_URL", "https://github.com/SirXavor/InfraServer.git")
